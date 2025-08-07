55bc8d8d2c2ca3311e6144b6f1706c7d1665b187
Tatiana Mosquera <atatianamosquera@gmail.com>
2025-08-07 17:18:39 -0500

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 to-purple-500 text-white">
+      <h1 className="text-5xl font-bold mb-4">¡Hola, soy Tatiana!</h1>
+      <p className="text-xl">Bienvenido a mi landing page</p>
+    </section>
+  );
+}
