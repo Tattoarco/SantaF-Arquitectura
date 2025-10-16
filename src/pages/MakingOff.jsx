6c0373dd208611900cc2f7c8c0e632e487c789d6
Tatiana Mosquera <atatianamosquera@gmail.com>
2025-10-16 01:52:50 -0500

    Change MakingOff

--- src/pages/MakingOff.jsx
+++ src/pages/MakingOff.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/navbar";
 
 export default function MakingOff() {
   return (
-    <section className="h-screen bg-slate-900 text-white ">
+    <section className="bg-slate-900 text-white ">
       <div className=" w-full h-20">
         <Navbar />
       </div>
@@ -11,7 +11,7 @@ export default function MakingOff() {
       <div className="">
         <h2 className="text-4xl md:text-5xl font-bold mb-12 tracking-wider text-center">Making Off</h2>
         <div className="w-full max-w-4xl aspect-video mb-8">
-          <iframe className="w-full h-full rounded-xl shadow-lg" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="Video de YouTube 1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+          <iframe width="700" height="500" className="rounded-xl shadow-lg" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="Video de YouTube 1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </div>
       </div>
 
